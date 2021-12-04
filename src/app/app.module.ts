cb6cf78b27d8dc9556c61c08683aa75e05d21129
Oscar Saca <oscarsaca88@gmail.com>
2021-12-04 12:23:28 -0600

    reorganize file structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,15 @@ import { HeaderComponent } from './components/header/header.component';
 import { LogoComponent } from './components/logo/logo.component';
 import { NavigationLinkComponent } from './components/navigation-link/navigation-link.component';
 import { SkipToContentComponent } from './components/skip-to-content/skip-to-content.component';
-import { MainstageComponent } from './components/mainstage/mainstage/mainstage.component';
+import { MainstageComponent } from './components/mainstage/mainstage.component';
 import { FilmLinesComponent } from './components/mainstage/film-lines/film-lines.component';
 import { BackgroundPlanetsComponent } from './components/mainstage/background-planets/background-planets.component';
 import { MountainsComponent } from './components/mainstage/mountains/mountains.component';
 import { InfoComponent } from './components/mainstage/header/info/info.component';
 import { TaglineComponent } from './components/mainstage/header/tagline/tagline.component';
-import { FooterComponent } from './components/footer/footer/footer.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { CopyrightComponent } from './components/footer/copyright/copyright.component';
-import { SocialLinkComponent } from './components/footer/social-link/social-link.component';
+import { SocialLinkComponent } from './components/social-link/social-link.component';
 
 @NgModule({
   declarations: [

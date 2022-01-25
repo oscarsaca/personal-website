abb1da6b9b17090035844ffd1048cc67a64f8ded
Oscar Saca <oscarsaca88@gmail.com>
2022-01-25 15:16:25 -0600

    update back to top component selector

--- src/app/components/back-top-top/back-top-top.component.ts
+++ src/app/components/back-top-top/back-top-top.component.ts
@@ -7,7 +7,7 @@ import {
 } from '@angular/core';
 
 @Component({
-  selector: 'app-back-top-top',
+  selector: 'app-back-to-top',
   templateUrl: './back-top-top.component.html',
   styleUrls: ['./back-to-top.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
